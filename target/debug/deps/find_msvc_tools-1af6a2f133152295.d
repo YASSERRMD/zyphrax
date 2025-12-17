be1d8f39c16f092d7266c9e661204c5cd8359525
YASSERRMD <mdyasser@Mohameds-MacBook-Air.local>
2025-12-17 18:47:42 +0400

    Phase 8: Rust FFI Wrapper

---
+++ target/debug/deps/find_msvc_tools-1af6a2f133152295.d
@@ -0,0 +1,9 @@
+/Users/mdyasser/Documents/projects/zyphrax/zyphrax/target/debug/deps/find_msvc_tools-1af6a2f133152295.d: /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/lib.rs /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/find_tools.rs /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/tool.rs
+
+/Users/mdyasser/Documents/projects/zyphrax/zyphrax/target/debug/deps/libfind_msvc_tools-1af6a2f133152295.rlib: /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/lib.rs /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/find_tools.rs /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/tool.rs
+
+/Users/mdyasser/Documents/projects/zyphrax/zyphrax/target/debug/deps/libfind_msvc_tools-1af6a2f133152295.rmeta: /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/lib.rs /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/find_tools.rs /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/tool.rs
+
+/Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/lib.rs:
+/Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/find_tools.rs:
+/Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.5/src/tool.rs:
---
+++ target/debug/deps/shlex-7ca1bf978857717c.d
@@ -0,0 +1,8 @@
+/Users/mdyasser/Documents/projects/zyphrax/zyphrax/target/debug/deps/shlex-7ca1bf978857717c.d: /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
+
+/Users/mdyasser/Documents/projects/zyphrax/zyphrax/target/debug/deps/libshlex-7ca1bf978857717c.rlib: /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
+
+/Users/mdyasser/Documents/projects/zyphrax/zyphrax/target/debug/deps/libshlex-7ca1bf978857717c.rmeta: /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
+
+/Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs:
+/Users/mdyasser/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs:
